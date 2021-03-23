68b0c706b4fbc1e9adfa9eac0ee0dd1555c8b99f
HAMISH SANSOM <103599187@student.swin.edu.au>
2021-03-23 14:42:30 +1100

    repition

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Repition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many times do you want to print that name.");
+            string num = Console.ReadLine();
+            int intnum = int.Parse(num);
+
+            int counter = 0;
+            
+            while (counter < intnum) 
+            {
+                Console.WriteLine("please enter your name");
+                string name = Console.ReadLine();
+                Console.WriteLine(name);
+                //counter = counter + 1;
+                counter++;
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
